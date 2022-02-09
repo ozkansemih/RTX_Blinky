4d5c3944c3c48c85ddbeb78e37b32f422b332d28
ozkansemih <semihozkan_96@hotmail.com>
2022-02-09 20:54:17 +0300

    first commit

---
+++ RTE/_LPC1857_Flash/RTE_Components.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'LPC1857 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+
+
+#endif /* RTE_COMPONENTS_H */
